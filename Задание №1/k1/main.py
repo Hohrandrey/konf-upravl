190af978ebe107893e6c26674e6ec5aefc8b3a8e
Hohrandrey <EnderBro_2005@mail.ru>
2024-11-20 09:20:37 +0300

    start rework

--- Задание №1/k1/main.py
+++ Задание №1/k1/main.py
@@ -9,24 +9,27 @@ def main():
     if len(argv) > 1:
         config_file = argv[1]
 
-    try:
-        with open(config_file, 'r') as file:
-            reader = csv.reader(file, delimiter=';')
-            for line in reader:
-                if len(line) >= 2:
-                    key, value = line[0].strip(), line[1].strip()
-                    if key == "username":
-                        user_name = value
-                    elif key == "compname":
-                        computer_name = value
-                    elif key == "zippath":
-                        zip_path = value
-                    elif key == "startpath":
-                        start_path = value
-            with ZipFile(zip_path, 'a') as file_system:
-                Window(MyTerminal(user_name, computer_name, zip_path, start_path, file_system)).start_polling()
-    except FileNotFoundError:
-        print(f"Файл {config_file} не найден.")
+        try:
+            with open(config_file, 'r') as file:
+                reader = csv.reader(file, delimiter=';')
+                for line in reader:
+                    if len(line) >= 2:
+                        key, value = line[0].strip(), line[1].strip()
+                        if key == "username":
+                            user_name = value
+                        elif key == "compname":
+                            computer_name = value
+                        elif key == "zippath":
+                            zip_path = value
+                        elif key == "startpath":
+                            start_path = value
+                with ZipFile(zip_path, 'a') as file_system:
+                    Window(MyTerminal(user_name, computer_name, zip_path, start_path, file_system)).start_polling()
+        except FileNotFoundError:
+            print(f"Файл {config_file} не найден.")
+    else:
+        print("Не задан конфигурационный файл")
+
 
 
 if __name__ == '__main__':
